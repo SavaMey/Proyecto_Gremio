60f4f8cf72c7942ea788793b9c3db0831ca43552
SavaMey <gabriel.ibarrag19@gmail.com>
2026-05-02 00:46:17 -0400

    first commit

---
+++ src/main/java/com/example/proyecot_gremio/ProyecotGremioApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecot_gremio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyecotGremioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyecotGremioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyecot_gremio/ProyecotGremioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecot_gremio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyecotGremioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
